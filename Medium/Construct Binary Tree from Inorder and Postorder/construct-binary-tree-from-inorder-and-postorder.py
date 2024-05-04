02252a5716a6a7ca29b4e752ded9a47b95457fb7
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2024-05-04 20:22:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.py
@@ -0,0 +1,74 @@
+#User function Template for python3
+
+'''
+class Node:
+            def __init__(self, data):
+                self.data = data
+                self.left = self.right = None
+'''
+
+#Function to return a tree created from postorder and inoreder traversals.
+class Solution:
+    def buildTree(self,In, post, n):
+        if len(In):
+            node = Node(post.pop())
+            ind = In.index(node.data)
+            node.right = self.buildTree(In[ind + 1:],post,n)
+            node.left = self.buildTree(In[:ind],post,n)
+            
+            return node
+        else:
+            return None
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+from collections import  defaultdict
+
+#Contributed by : PranchalK
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+
+# Helper function that allocates  
+# a new node  
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+# This funtcion is here just to test  
+def preOrder(node):
+    if node == None:
+        return
+    print(node.data, end=" ")
+    preOrder(node.left)
+    preOrder(node.right)
+    
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())  # number of nodes in tree
+        in_order = list(map(int, input().strip().split()))  # parent child info in list
+        post_order = list(map(int, input().strip().split()))  # parent child info in list
+        ob = Solution()
+        preOrder(ob.buildTree(in_order,post_order,n))
+        print()
+
+
+# } Driver Code Ends
