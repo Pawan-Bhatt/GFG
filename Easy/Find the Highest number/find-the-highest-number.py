0ce57ac3fcb4d77142039aa132969a825e5eb09c
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2024-05-18 22:56:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the Highest number/find-the-highest-number.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+from typing import List
+
+class Solution:
+	def findPeakElement(self, a):
+		a.sort()
+		return a[len(a) - 1]
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        n = int(input())
+        a = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.findPeakElement(a)
+        print(ans)
+
+# } Driver Code Ends
